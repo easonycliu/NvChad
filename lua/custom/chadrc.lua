fafc24e907bd7b75174f3baf8a1e6d0250685d24
easonycliu <easonliu@ucla.edu>
2025-04-25 15:46:13 -0700

    remove lsp

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'nightfox' }
+M.ui = { theme = 'onedark' }
 M.plugins = 'custom.plugins'
 
 return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -142,16 +142,6 @@ local default_plugins = {
     end,
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    init = function()
-      require("core.utils").lazy_load "nvim-lspconfig"
-    end,
-    config = function()
-      require "plugins.configs.lspconfig"
-    end,
-  },
-
   -- load luasnips + cmp related in insert mode only
   {
     "hrsh7th/nvim-cmp",
